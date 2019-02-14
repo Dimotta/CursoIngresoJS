27a222ea707dcb8d8a51c6784e2608fa3431bfed
Dimotta <jddimotta@gmail.com>
2019-02-14 19:34:54 -0300

    primero finalizado

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,23 @@
 
 function mostrar()
 {
+	var mascota_uno;
+	var mascota_dos;
+	var peso_uno;
+	var peso_dos;
+	var suma;
+
+	peso_uno=parseInt(peso_uno);
+	peso_dos=parseInt(peso_dos);
+
+	mascota_uno=hehe;
+	mascota_dos=hihi;
+	peso_uno=15;
+	peso_dos=14;
+
+	result=peso_uno+peso_dos;
+	suma=result;
+
+	alert("tenes dos mascotas " mascota_uno "y  " mascota_dos ", que pesan" peso_uno "y" peso_dos ",la suma de los kilos es " suma );
 
 }
