e1ead4962ddc07b002636dbf8ade3295dcbf6851
Dimotta <jddimotta@gmail.com>
2019-02-21 21:18:45 -0300

    ejercicios realizados

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,12 @@
 function mostrar()
 {
+	var preciohab;
+	var	pago;
+	var paquete;
+	var descuento;
+
+	preciohab=prompt("Ingrese el Precio de su Habitacion");
+	paquete=prompt("Ingrese su Paquete Comprado");
+	pago=prompt("Ingrese su Tipo de Pago");
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,42 @@
 function mostrar()
 {
+	var numero_uno;
+	var numero_dos;
+	var numero_tres;
+	var numero_cuatro;
+	var compra;
+	var mensaje;
 
+	
+	numero_uno=prompt("Ingrese un numero");
+	numero_dos=prompt("Ingrese un numero");
+	numero_tres=prompt("Ingrese un numero");
+	numero_cuatro=prompt("Ingrese un numero");
+
+	numero_uno=parseInt(numero_uno);
+	numero_dos=parseInt(numero_dos);
+	numero_tres=parseInt(numero_tres);
+	numero_cuatro=parseInt(numero_cuatro);
+
+	compra=numero_uno+numero_dos+numero_tres+numero_cuatro;
+
+	if(compra>99)
+	{
+		mensaje=compra*0.90
+	}
+	else
+	{
+		if(compra>50)
+		{
+			mensaje=compra*0.95
+		}
+		else
+		{
+			mensaje=compra*1.15
+		}
+	
+	}
+
+	alert("Tu compra es de " +mensaje);
 }
+
