eab47120f9d13df91418f8da30fadc45a1c03550
Dimotta <jddimotta@gmail.com>
2019-03-01 22:09:33 -0300

    1 y casi el 2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,56 @@
 function mostrar()
 {
+	var pais;
+	var superficie;
+	var respuesta;
+	var contador;
+	var superficieImpar;
+	var superficieMenor;
+	var superficieIgual;
 
+	respuesta="si";
+	contador=0;
+	superficieImpar=0;
+	superficieMenor=0;
+	superficieIgual=0;
+
+	while(respuesta=="si")
+	{
+		pais=prompt("Ingrese un pais");
+		superficie=prompt("Ingrese una superficie");
+		superficie=parseInt(superficie);
+
+		while(superficie<0 || superficie>1000)
+		{
+			superficie=prompt("Error,Ingresar otra superficie");
+		}
+
+		respuesta=prompt("Ingrese si para continuar");
+
+		if (superficie%2==1)
+		{
+			superficieImpar++;
+		}
+		if (superficie<100)
+		{
+			superficieMenor++;
+		}
+		if (superficie==100)
+		{
+			superficieIgual++;
+		}
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+	alert (superficieMenor);
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -1,4 +1,17 @@
 function mostrar()
 {
+	var pais;
+	var superficie;
+	var contador;
+	var respuesta;
 
+	contador=0;
+	respuesta="si";
+
+	while(respuesta=="si")
+	{
+		pais=prompt("Ingrese un pais");
+
+		superficie=prompt("Ingrese una superficie");
+	}
 }
