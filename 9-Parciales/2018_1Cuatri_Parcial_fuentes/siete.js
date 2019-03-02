ee32260007eafe08b4bc256efced7cfdd7d7d3d0
Dimotta <jddimotta@gmail.com>
2019-03-01 21:01:31 -0300

    ej 1 del modelo de examen

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,42 +1,81 @@
 function mostrar()
 {
+	var	velocidad;
+	var combustible;
 	var contador;
-	var notas;
+	var promedioVelocidad;
+	var velocidadBaja;
+	var velocidadAlta;
 	var acumulador;
-	var sexo;
-	var cantidadvarones;
+	var combustibleMin;
+	var combustibleMax;
+	var combustibleLiquido;
+	var maximo;
 
+	combustibleLiquido=0;
+	acumulador=0;
 	contador=0;
+	promedioVelocidad=0;
+	maximo=0;
 
-	while(contador<5) 
+	while(contador<5)
 	{
-		contador=contador+1;
-		notas=prompt("Ingrese su nota");
-		sexo=prompt("Ingrese si es f o m");
 
-		if (sexo!="f" && sexo!="m")
-		{
-			prompt("Ingrese si es f o m")
-		}
-		if (notas>-1 && notas<11)
-		{
-			notas=parseInt(notas);
-			notas=prompt("Ingrese su nota")
-			acumulador=acumulador+notas;
+		contador=contador+1
 
-		}
-		if (notas>5 && sexo!="m") 
-		{
+		velocidad=prompt("Ingrese su Velocidad");
+		velocidad=parseInt(velocidad);
+		promedioVelocidad=promedioVelocidad+velocidad;
+		maximo=velocidad+1;
 
+		while(velocidad<0 || velocidad>250)
+		{
+			velocidad=prompt("Error,Ingrese su velocidad");
 		}
 
+		combustible=prompt("Ingrese su Combustible");
 
-		alert
+		while(combustible!="s" && combustible!="l")
+		{
+			combustible=prompt("Error,Ingrese su combustible");
+		}
+
+		if (contador==1)
+		{
+			velocidadBaja=velocidad;
+			combustibleMin=combustible;
+		}
+		else
+		{
+			if (velocidad<velocidadBaja)
+			 {
+			 	velocidadBaja=velocidad;
+			 	combustibleMin=combustible;
+			 }
+		}
+
+		if (contador==1)
+		{
+			velocidadAlta=velocidad;
+			combustibleMax=combustible;
+		}
+		else
+		{
+			if (velocidad>velocidadAlta && combustible=="s") 
+			{
+				velocidadAlta=velocidad;
+				combustibleMax=combustible;
+			}
+		}
+
+		if (velocidad>99 && combustible=="l")
+		{
+			combustibleLiquido=combustibleLiquido+1;
+		}
 
 
 	}
+		promedioVelocidad=promedioVelocidad/contador;
 
-
-
-
+		alert("El promedio de velocidad es "+promedioVelocidad+" ,la velocidad mas baja es "+velocidadBaja+" y su combustible es "+combustibleMin+" , la cantidad de combustibles liquidos que superan los 100km es "+combustibleLiquido+" y la velocidad mas alta del combustible solido es "+velocidadAlta);
 }
